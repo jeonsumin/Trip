44b05506d3ea1332f14b6173f3c9d0fb915dd6bd
jeonsumin <soomis7@naver.com>
2022-02-04 23:17:07 +0900

    스크롤뷰안에서 테이블뷰가 자연스럽게 스크롤되도록 수정

--- Trip/Controller/Home/HomeViewController.swift
+++ Trip/Controller/Home/HomeViewController.swift
@@ -14,23 +14,40 @@ class HomeViewController: UIViewController {
     private let scrollview = UIScrollView()
     private let contentView = UIView()
     private let stackView = UIStackView()
-    
+    private let tableView = UITableView()
+//    private var tableViewHegit = ConstraintItem
     
     private let curationSectionView = CurationSectionView(frame: .zero)
     private let placesToVisitAroundSectionView = PlacesToVisitAroundSectionView(frame: .zero)
     private let locationSectionView = UIView()
-    private let contentSectionView = ContentSectionView(frame: .zero)
+//    private let contentSectionView = ContentSectionView(frame: .zero)
     
     
     //MARK: -LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupTableView()
         setupUI()
+        
     }
 
 
 }
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeViewContentTableViewCell", for: indexPath)
+        cell.backgroundColor = .lightGray
+        
+        return cell
+    }
+    
+    
+}
+
 
 //MARK: - Private Function
 private extension HomeViewController {
@@ -43,7 +60,9 @@ private extension HomeViewController {
         stackView.distribution = .equalSpacing
         stackView.spacing = 0
         
+        scrollview.showsVerticalScrollIndicator = false
         view.addSubview(scrollview)
+        scrollview.bounces = false
         scrollview.snp.makeConstraints{
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             $0.leading.trailing.bottom.equalToSuperview()
@@ -61,10 +80,37 @@ private extension HomeViewController {
         
         [curationSectionView,
          placesToVisitAroundSectionView,
+         tableView
 //         locationSectionView,
-         contentSectionView
+//         contentSectionView
         ].forEach{
             stackView.addArrangedSubview($0) }
         
+        
+        tableView.bounces = true
+        tableView.snp.makeConstraints{
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.top.equalTo(placesToVisitAroundSectionView.snp.bottom).offset(25)
+//            $0.height.equalTo(tableView.snp.width)
+            $0.height.equalTo(1000)
+            
+        }
+        
+    }
+    func setupTableView(){
+        tableView.dataSource = self
+//        tableViewHegit = tableView.heightAnchor
+        tableView.showsVerticalScrollIndicator = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "HomeViewContentTableViewCell")
+    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(scrollView)
+        if scrollview == self.scrollview {
+            tableView.isScrollEnabled = (scrollview.contentOffset.y > 600 )
+        }
+        if scrollview == tableView {
+            tableView.isScrollEnabled = (tableView.contentOffset.y > 0 )
+        }
     }
 }
